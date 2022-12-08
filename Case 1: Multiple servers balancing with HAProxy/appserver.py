dbb14c33bef4aac50e8e982ee9f098c7319534e6
madhumantimaiti <118077522+madhumantimaiti@users.noreply.github.com>
2022-12-08 13:35:51 -0700

    Update appserver.py

--- Case 1: Multiple servers balancing with HAProxy/appserver.py
+++ Case 1: Multiple servers balancing with HAProxy/appserver.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 @app.route('/')
 def index():
         print(request.headers)
-        return "Welcome to server 1 with  %s" %portno
+        return "Welcome to server running with  %s" %portno
         
 if __name__ =="__main__":
        app.run(port=sys.argv[1])        

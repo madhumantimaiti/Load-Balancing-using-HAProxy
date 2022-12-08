46fc34d56114766884cbbe27d8f5d77c412cbdf2
madhumantimaiti <118077522+madhumantimaiti@users.noreply.github.com>
2022-12-08 16:45:56 -0700

    Update nodeserver.js

--- Case 3: Load Balancing Node JS servers/nodeserver.js
+++ Case 3: Load Balancing Node JS servers/nodeserver.js
@@ -1,11 +1,8 @@
-// File /srv/server.js
 var http = require('http');
 
 function serve(ip, port)
 {
         http.createServer(function (req, res) {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write(JSON.stringify(req.headers));
             res.end("\nServer listening on port  "+ip+":"+port+"\n");
         }).listen(port, ip);
         console.log('Listening on port and IP  at http://'+ip+':'+port+'/');
